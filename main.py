0ee37349f276babb81b08142e32acd4518c1b9a9
yangjae33 <wogurrhtn@gmail.com>
2020-08-26 19:38:03 +0900

    ADD FEATURE: STATUS & 완료 버튼 정의

--- main.py
+++ main.py
@@ -15,14 +15,27 @@ from requests.exceptions import HTTPError
 
 tl = Timeloop()
 
-#시나리오별 state
+# 시나리오별 State
+# 0."DEACTIVE" : 처음 확인버튼으로 GPS정보 송신
+# 0-1. "DEACTIVATE_CHK" : GPS 확인질문
+# 1."ROUTE_NAME" : 노선 번호 입력
+# 1-1. "ROUTE_NAME_CHK" : 노선 번호 입력 확인질문
+# 2."STN_ID" : 해당 노선을 지나는 정류장 중 하나 입력
+# 2-1. "STN_ID_CHK" : 정류장 확인질문
+# 3."RUNNING" : 승차 직후 ~ 하차
+
 STATE = "ROUTE_NAME" #ROUTE_NAME, DESTINATION, ...
 
 # 현재 출력되고 있는 점자 숫자
 SELECTED_NUM =0
 
 #고른 노선
-SELECTED_ROUNTE_NAME = ""
+SELECTED_ROUTE_NAME = ""
+
+#고른 하차 정류장
+SELECTED_STN_NAME = ""
+SELECTED_STN_ID = ""
+
 #전역 변수
 host="http://114.70.21.89:1337"
 
@@ -104,15 +117,89 @@ def switch_next_callback():
         print("state: ROUTE_NAME")
 
 def switch_save_callback():
-    global SELECTED_ROUNTE_NAME
+    global SELECTED_ROUTE_NAME
     print("switch_save_callback")
     if STATE == "ROUTE_NAME":
-        SELECTED_ROUNTE_NAME += str(SELECTED_NUM)
+        SELECTED_ROUTE_NAME += str(SELECTED_NUM)
 
 def switch_done_callback():
+
+    # 승차 정류장 설정
+    if STATE == "DEACTIVE":
+        global SELECTED_STN_NAME
+        global SELECTED_STN_ID
+        # GPS보내기
+        pos_x,pos_y=hw.gps()
+        dict_data = dict()
+        dict_data['pos_x'] = pos_x
+        dict_data['pos_y'] = pos_y
+        url = host+'/api/gps/stn'
+        res = requests.post(url,data=json.dumps(dict_data))
+
+        SELECTED_STN_NAME = res.json()[0]['stn_name']
+        SELECTED_STN_ID = res.json()[0]['stn_id']
+
+        print("승차 위치로 \'"+SELECTED_STN_NAME+"\'가 맞습니까?\n")
+        STATE = "DEACTIVATE_CHK"
+
+    # 승차 정류장 설정 확인
+    elif STATE == "DEACTIVATE_CHK":
+        print("승차 정류장 설정 완료")
+        STATE = "ROUTE_NAME"
+
+    # 노선 입력
+    elif STATE == "ROUTE_NAME":
+        global SELECTED_ROUTE_NAME
+        if SELECTED_ROUTE_NAME != "":
+            print("승차 노선이 "+SELECTED_ROUTE_NAME+"가 맞습니까?\n")
+            STATE = "ROUTE_NAME_CHK"
+        else:
+            STATE = "ROUTE_NAME"
+   
+    # 노선 입력 확인
+    elif STATE == "ROUTE_NAME_CHK":
+        print("노선 설정 완료")
+        STATE = "STN_NAME"
+
+    # 하차벨 예약
+    elif STATE == "STN_NAME":
+    
+        global USER_ID
+        global SELECTED_STN_ID
+        global SELECTED_ROUTE_NAME
+
+        if SELECTED_STN_ID == "" or SELECTED_ROUTE_NAME == "":
+            STATE = "STN_NAME"
+            print("유효하지 않은 역 또는 노선")
+            return
+
+        print("노선 " + SELECTED_ROUTE_NAME + " 과 정류장 이름 "+SELECTED_STN_ID+" 가 맞습니까? ")
+        STATE = "STN_NAME_CHK"
+
+    # 하차벨 예약 확인
+    elif STATE == "STN_NAME_CHK":
+        dict_data = dict()
+        dict_data['user_id'] = USER_ID
+        dict_data['stn_id'] = SELECTED_ROUTE_NAME
+        dict_data['route_nm'] = SELECTED_STN_ID
+        url = host+'/buzzer/register'
+        data = requests.post(url,data=json.dumps(dict_data))
+        status_data = data.status_code
+
+        if str(status_data) == "200":
+            # TODO : 몇분후에 도착하는지 알림
+            print("n분 후에 도착 예정입니다. ")
+            STATE = "RUNNING"
+        else :
+            print("유효하지 않은 요청")
+            STATE = "STN_NAME"
+            SELECTED_STN_ID = ""
+        
+    elif STATE == "RUNNING":
+        STATE = "DEACTIVATE"
+        # TODO : 종료(?)
+
     print("switch_done_callback")
-
-
 
 def main():
     # GPIO 초기화 (GPS,Solenoid, Switch) - 이벤트 핸들러 등록
